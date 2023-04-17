ee1c1bb12083ca8a299ce5927de5ab16b6cc77a7
Armin Barzegar Amin <armin.barzegaramin@student.ap.be>
2023-04-17 10:41:40 +0200

    rows fixed

---
+++ src/components/velostation.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+
+function velostation(props) {
+    return (
+
+        <div className={styles.card}>
+                <h4>{props.station.name}</h4>
+                <p>{props.station.extra.address}</p>
+                <div className={styles.box}>
+                  <Image
+                    src="/veloIcon.png"
+                    alt="gele fiets icoon"
+                    className={styles.veloIcon}
+                    width={40}
+                    height={24}
+                    priority
+                  />
+                  <p>{props.station.free_bikes}</p>
+                  <Image
+                    src="/pinIcon.png"
+                    alt="gele pin icoon"
+                    className={styles.veloIcon}
+                    width={10}
+                    height={20}
+                    priority
+                  />
+                  <p>999m</p>
+                </div>
+              </div>
+
+    );
+  }
+  
+  export default velostation;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Velodashboard from '@/components/Velodashboard.js'
+import Velostation from '@/components/velostation.js'
 import {useState} from 'react';
 import useNetwork from '@/data/network'
 
@@ -55,31 +56,7 @@ export default function Home() {
         <div className={styles.cards}>
           <div className={styles.cards_content}>
             {stations.map(station =>  
-              <div key={station.id}>
-                <h4>{station.name}</h4>
-                <p>{station.extra.address}</p>
-                <div className={styles.box}>
-                  <Image
-                    src="/veloIcon.png"
-                    alt="gele fiets icoon"
-                    className={styles.veloIcon}
-                    width={40}
-                    height={24}
-                    priority
-                  />
-                  <p>{station.free_bikes}</p>
-                  <Image
-                    src="/pinIcon.png"
-                    alt="gele pin icoon"
-                    className={styles.veloIcon}
-                    width={10}
-                    height={20}
-                    priority
-                  />
-                  <p>999m</p>
-                </div>
-                <h1>----------------------------------</h1>
-              </div>
+              <Velostation station={station} key={station.id}/>
             )}
           </div>
         </div>
