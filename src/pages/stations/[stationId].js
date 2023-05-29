546ea352557d841e24ccc0013ea858f3f7329ed7
Armin Barzegar Amin <armin.barzegaramin@student.ap.be>
2023-05-29 13:31:09 +0200

    buttons and layout (styling)

--- src/pages/stations/[stationId].js
+++ src/pages/stations/[stationId].js
@@ -23,29 +23,62 @@ export default function Home() {
       </header>
 
       <main>
-        <div className={styles.cards}>
-        <h4 className={styles.haavier}>{station.name}</h4>
-        <p className={styles.pee}>{station.extra.address}</p>
-        <div className={styles.box}>
+        <div className={styles.veloBox}>
+        <div className={styles.veloFoto}>
           <Image
-            src="/veloIcon.png"
-            alt="gele fiets icoon"
-            width={34}
-            height={17}
-            priority
+            src="/velo-haadee-foto.jpeg"
+            alt="background-picture-of-velo-antwerpen-bikes"
+
+            priority          
+            layout="fill"
+            objectFit="cover"
           />
-          <div  className="bikesEnAfstand">
-            <h6 className={styles.haazes}>{station.free_bikes}</h6>
-          </div>
-          <Image
-            src="/pinIcon.png"
-            alt="gele pin icoon"
-            width={8}
-            height={16}
-            priority
-          />
-          <h6 className={styles.haazes}>{station.distance * 1000}m</h6>
         </div>
+        </div>
+
+        <div className={styles.cards}>
+          <div className={styles.pusher}>
+            <Image
+              src="/location-icon.png"
+              alt="gele fiets icoon"
+              width={16}
+              height={20}
+              priority
+            />
+          </div>
+          <h4 className={styles.haavier}>{station.name}</h4>
+          <p className={styles.pee}>{station.extra.address}</p>
+          <div className={styles.boxDrie}>
+            <div className={styles.boxTwee}>
+              <Image
+                src="/veloIcon.png"
+                alt="gele fiets icoon"
+                width={34}
+                height={17}
+                priority
+              />
+              <div>
+                <h6 className={styles.haazes}>{station.free_bikes}</h6>
+              </div>
+              <Image
+                src="/pinIcon.png"
+                alt="gele pin icoon"
+                width={8}
+                height={16}
+                priority
+              />
+              <h6 className={styles.haazes}>{station.distance * 1000}m</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.buttons}>
+          <div>
+            <button className={styles.koop}><span>Koop</span> een dagticket</button>
+          </div>
+          <div>
+            <button className={styles.reserveer}><span>Reserveer</span> je fiets</button>
+          </div>
         </div>
 
         </main>
